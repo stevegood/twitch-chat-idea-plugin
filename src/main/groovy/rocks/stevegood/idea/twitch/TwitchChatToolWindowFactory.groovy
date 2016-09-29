048bb2f8c5f5c5d3eeef84244012a98f36618895
Steve Good <steve.good@am.sony.com>
2016-09-29 16:47:06 -0700

    Changed the chat mechanism so that it uses the markup builder.

--- src/main/groovy/rocks/stevegood/idea/twitch/TwitchChatToolWindowFactory.groovy
+++ src/main/groovy/rocks/stevegood/idea/twitch/TwitchChatToolWindowFactory.groovy
@@ -5,6 +5,7 @@ import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.Content
 import com.intellij.ui.content.ContentFactory
+import groovy.xml.MarkupBuilder
 import org.jetbrains.annotations.NotNull
 import tv.extrememoderation.twitch.chat.TwitchBot
 
@@ -15,7 +16,6 @@ class TwitchChatToolWindowFactory implements ToolWindowFactory {
     private ToolWindow twitchTW
     private TwitchChatToolWindow chatWindow
     private List messages
-    private def bot
 
     TwitchChatToolWindowFactory() {
         messages = []
@@ -25,9 +25,6 @@ class TwitchChatToolWindowFactory implements ToolWindowFactory {
         }
 
         chatWindow.sendChatButton.addActionListener sendChatAction
-
-        bot = new TwitchBot('ExtremeModeration', 'oauth:wb6kzrwpb5erg067hdtquffnv5uqpo')
-        bot.startBot()
     }
 
     @Override
@@ -52,24 +49,20 @@ class TwitchChatToolWindowFactory implements ToolWindowFactory {
     }
 
     private void updateChat() {
-        def parts = [
-            '<html>',
-            '<head></head>',
-            '<body>'
-        ]
-
-        messages.each { message ->
-            parts << '<p>'
-            parts << "<small>${message.date}</small>"
-            parts << "<strong> ${message.author}: </strong>"
-            parts << "<span>${message.text}</span>"
-            parts << '</p>'
+        StringWriter writer = new StringWriter()
+        new MarkupBuilder(writer).html {
+            head ''
+            body {
+                messages.each { message ->
+                    p {
+                        small message.date
+                        strong " $message.author: "
+                        span message.text
+                    }
+                }
+            }
         }
 
-        parts += [
-            '</body></html>'
-        ]
-
-        chatWindow.chatTextPane.text = parts.join ''
+        chatWindow.chatTextPane.text = writer.toString()
     }
 }
